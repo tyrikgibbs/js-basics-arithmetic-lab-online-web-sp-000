75beb2c7d3235b0d590a5baa7c1389d27ae550d1
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-01-22 03:09:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ let newID = 1000000000 + oldID;
 let ageIsValid = Number.isInteger(currentAge)
 let randomNumber = Math.floor(Math.random() * 20 ) + 1
 let randomInteger = Math.floor(randomNumber)
+let randomUserID = Math.floor(Math.random() * 20 ) + 1000000000
